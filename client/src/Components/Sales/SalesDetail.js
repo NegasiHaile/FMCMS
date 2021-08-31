6b718fc61d6d3b47355afc8ce71a0ba6f3ddfbe0
unknown <you@example.com>
2021-08-31 17:18:49 +0300

    Error fixed in sales detail

--- client/src/Components/Sales/SalesDetail.js
+++ client/src/Components/Sales/SalesDetail.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { GlobalState } from "../../GlobalState";
 
@@ -10,8 +10,8 @@ function SalesDetail() {
   const [callbackMachines, setCallbackMachines] = state.MachineAPI.callback;
   const [callbackBusiness, setCallbackBusiness] = state.BusinessAPI.callback;
   const [callbackSales, setCallbackSales] = state.SalesAPI.callback;
-const [sales]
-  useEffect(() => {}, [params.id, Sales, ]);
+
+  useEffect(() => {}, [params.id, Sales]);
   return (
     <div>
       <h1>This is The sales detail {params.id}</h1>
