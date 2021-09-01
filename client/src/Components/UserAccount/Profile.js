d3bfc25f90fdd7c5afe5b803b0d4c89d8749f109
unknown <you@example.com>
2021-09-01 09:01:10 +0300

    User profile set to desplay dynamical data

--- client/src/Components/UserAccount/Profile.js
+++ client/src/Components/UserAccount/Profile.js
@@ -1,19 +1,37 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { GlobalState } from "../../GlobalState";
 function Profile() {
   const state = useContext(GlobalState);
-  const [user] = state.UserAPI.User;
+  const params = useParams();
+  const [users] = state.UsersAPI.users;
+  const [userDetail, setUserDetail] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+      const user = users.find((filteredUser) => filteredUser._id === params.id);
+      setUserDetail(user);
+    } else {
+    }
+  }, [params.id, users]);
 
   return (
     <div>
-      <h2>THis is the user profile</h2>
-      <div>
-        <h4>
-          {user.fName} {user.mName}
-          {user.lName}
-        </h4>
-        <h4>{user.email}</h4>
-      </div>
+      {userDetail ? (
+        <>
+          <h5>
+            Full Name:{" "}
+            {userDetail.fName + " " + userDetail.mName + " " + userDetail.lName}{" "}
+          </h5>
+          <h5>Contacts: {userDetail.phoneNumber + " " + userDetail.email}</h5>
+
+          <div></div>
+        </>
+      ) : (
+        <div>
+          <h4>Bad routing, pleas navigate to your home page and come again.</h4>
+        </div>
+      )}
     </div>
   );
 }
