871d1aa04046c03c29b7e5d69570c2fe8d468013
unknown <you@example.com>
2021-09-08 09:49:31 +0300

    Heroku issue fixing t2

--- client/src/Components/Branch/BranchsList.js
+++ client/src/Components/Branch/BranchsList.js
@@ -1,4 +1,40 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import axios from "axios";
+import { GlobalState } from "../../GlobalState";
+import {
+  CButton,
+  CCard,
+  CCardHeader,
+  CCardBody,
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter,
+  CLabel,
+  CForm,
+  CRow,
+  CCol,
+  CFormGroup,
+  CInput,
+  CDataTable,
+  CLink,
+  CTooltip,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import Swal from "sweetalert2";
+
+const branchDetail = {
+  branchName: "",
+  city: "",
+  subCity: "",
+  kebele: "",
+  woreda: "",
+  buildingName: "",
+  officeNumber: "",
+  telephone: "",
+  email: "",
+};
 
 function BranchsList() {
   return (
