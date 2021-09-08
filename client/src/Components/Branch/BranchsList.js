fb15a065abb356347ee8e57d8905921a854a59dd
unknown <you@example.com>
2021-09-08 10:06:32 +0300

    Heroku issue fixing t4

--- client/src/Components/Branch/BranchsList.js
+++ client/src/Components/Branch/BranchsList.js
@@ -45,6 +45,89 @@ function BranchsList() {
   const [callback, setCallback] = state.branchAPI.callback;
   const [activeBranch, setActiveBranch] = useState("none");
   const [showModal, setShowModal] = useState(false);
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setBranch({ ...branch, [name]: value });
+  };
+
+  const sweetAlert = (type, text) => {
+    Swal.fire({
+      position: "center",
+      background: "#EBEDEF", // 2EB85C success // E55353 danger // 1E263C sidebar
+      icon: type,
+      text: text,
+      confirmButtonColor: "#3C4B64",
+      showConfirmButton: true,
+      // timer: 1500,
+    });
+  };
+
+  const onSubmitOpenBranch = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post("/branch/open_new", { ...branch });
+      sweetAlert("success", res.data.msg);
+      setShowModal(!showModal);
+      setCallback(!callback);
+    } catch (error) {
+      sweetAlert("error", error.response.data.msg);
+    }
+  };
+
+  const editBranchDetail = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(
+        `/branch/edit/${activeBranch}`,
+        { ...branch },
+        {
+          headers: { Authorization: token },
+        }
+      );
+      sweetAlert("success", res.data.msg);
+      setShowModal(!showModal);
+      setCallback(!callback);
+    } catch (error) {
+      sweetAlert("error", error.response.data.msg);
+    }
+  };
+
+  const deleteBranch = async (_id, branchName) => {
+    try {
+      Swal.fire({
+        title: "Delete?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#1E263C",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          const res = await axios.delete(`/branch/delete/${_id}`, {
+            headers: { Authorization: token },
+          });
+          Swal.fire("Deleted!", res.data.msg, "success");
+          setCallback(!callback);
+        }
+      });
+    } catch (error) {
+      sweetAlert("error", error.response.data.msg);
+    }
+  };
+
+  const branchTableFields = [
+    "branchName",
+    "city",
+    "subCity",
+    "kebele",
+    "woreda",
+    "buildingName",
+    "officeNumber",
+    "telephone",
+    "Actions",
+  ];
   return (
     <div>
       <h3>This is branchs list</h3>
