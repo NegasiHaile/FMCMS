4abc0c52fdd9948ae20879d6fdbd4b6f0de46a9b
unknown <you@example.com>
2021-09-20 17:12:36 +0300

    new model of machine pickup is created for mew design

---
+++ models/machinePickupModel.js
@@ -0,0 +1,108 @@
+const mongoose = require("mongoose");
+const ObjectID = mongoose.Schema.Types.ObjectID;
+
+const machinePickupModel = new mongoose.Schema(
+  {
+    branchID: {
+      type: ObjectID,
+      required: true,
+    },
+    salesID: {
+      type: ObjectID,
+      required: true,
+    },
+    businessId: {
+      type: ObjectID,
+      required: true,
+    },
+    machineID: {
+      type: ObjectID,
+      required: true,
+    },
+    memoryKey: {
+      type: Boolean,
+    },
+    drawer: {
+      type: Boolean,
+    },
+    paper: {
+      type: Boolean,
+    },
+    terminal: {
+      type: Boolean,
+    },
+    terminalAdapte: {
+      type: Boolean,
+    },
+    machine: {
+      type: Boolean,
+    },
+    SBookTerminal: {
+      type: Boolean,
+    },
+    SbookMachine: {
+      type: Boolean,
+    },
+    paperRoller: {
+      type: Boolean,
+    },
+    paperCover: {
+      type: Boolean,
+    },
+    machineAdapter: {
+      type: Boolean,
+    },
+    FDForm: {
+      type: Boolean,
+    },
+    sealNumber: {
+      type: Boolean,
+    },
+    MRCNumber: {
+      type: Boolean,
+    },
+    category: {
+      type: String, // annual/incident/return/waiting
+      required: true,
+    },
+    subCategory: {
+      type: String, // osite/onfron/inside/
+    },
+    clientReportedIssue: {
+      type: String,
+    },
+    technicianReportedIssue: {
+      type: String,
+    },
+    returnReason: {
+      type: String,
+    },
+    returnCertificate: {
+      type: String,
+    },
+    operationDate: {
+      type: Date, //  the operation complated date
+    },
+    annualNextMaintenanceDate: {
+      type: Date,
+    },
+    waitingDuration: {
+      type: String,
+    },
+    waitingCostPerMonth: {
+      type: String,
+    },
+    pickedupBy: {
+      type: String,
+    },
+    status: {
+      type: Date,
+      default: "New",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("MachinePickups", machinePickupModel);
