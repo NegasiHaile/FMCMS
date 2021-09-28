f88b38dec83932dd135fbc39cce16531bf98e99c
unknown <you@example.com>
2021-09-28 12:42:02 +0300

    Machine pickup list displayed in maintenance and returing partitions

--- client/src/Components/Maintenance/MaintenanceList.js
+++ client/src/Components/Maintenance/MaintenanceList.js
@@ -1,10 +1,92 @@
-import React from "react";
+import React, { useState, useContext, useEffect } from "react";
+import axios from "axios";
+import { GlobalState } from "../../GlobalState";
+import {
+  CButton,
+  CCard,
+  CCardBody,
+  CDataTable,
+  CLink,
+  CTooltip,
+} from "@coreui/react";
+import CIcon from "@coreui/icons-react";
+import Swal from "sweetalert2";
 
+const machinePickupsFields = [
+  "tradeName",
+  "serialNumber",
+  "machineModel",
+  "category",
+  "status",
+  "createdAt",
+  "branchName",
+  {
+    key: "Actions",
+    label: "Actions",
+    // _style: { width: "1%" },
+    sorter: false,
+    filter: false,
+  },
+];
 function MaintenanceList() {
+  const state = useContext(GlobalState);
+  const [user] = state.UserAPI.User;
+  const [pickupMachines] = state.MachinePickUpAPI.machinePickups;
+  const [returns, setReturns] = useState([]);
+  const [callbackMachinePickup, setCallbackMachinePickup] =
+    state.MachinePickUpAPI.callback;
+
+  useEffect(() => {
+    if (pickupMachines.length > 0) {
+      setReturns(
+        pickupMachines.filter(
+          (filteredPickUp) =>
+            (filteredPickUp.category === "annual" ||
+              filteredPickUp.category === "incident" ||
+              filteredPickUp.category === "information_change") &&
+            filteredPickUp.branchId === user.branch
+        )
+      );
+    }
+  }, [user, pickupMachines, setReturns]);
   return (
-    <div>
-      <h3> Maintenance list</h3>
-    </div>
+    <>
+      <CCard className=" shadow-sm">
+        <CCardBody>
+          <CDataTable
+            size="sm"
+            items={returns}
+            fields={machinePickupsFields}
+            tableFilter
+            columnFilter
+            itemsPerPageSelect
+            itemsPerPage={10}
+            hover
+            cleaner
+            sorter
+            pagination
+            scopedSlots={{
+              Actions: (pickup) => (
+                <td className="d-flex justify-content-between">
+                  <>
+                    <CLink
+                      className="text-info"
+                      to={`/pickup/detail/${pickup._id}`}
+                    >
+                      <CTooltip
+                        content={`See detail of this ${pickup.category} maintenance.`}
+                      >
+                        <CIcon name="cil-align-center" />
+                      </CTooltip>
+                    </CLink>
+                  </>
+                </td>
+              ),
+            }}
+          />
+        </CCardBody>
+      </CCard>
+    </>
   );
 }
 
