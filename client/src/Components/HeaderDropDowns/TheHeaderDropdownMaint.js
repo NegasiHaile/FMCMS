fcf5d250de22fe03cd66299d068316c509b9394a
unknown <you@example.com>
2021-10-16 17:08:08 +0300

    The badest bug is debugged successfuly, it was uneccessarly calling of callback in TheHeaderDropdownMaint.js

--- client/src/Components/HeaderDropDowns/TheHeaderDropdownMaint.js
+++ client/src/Components/HeaderDropDowns/TheHeaderDropdownMaint.js
@@ -14,7 +14,7 @@ const TheHeaderDropdownMaint = () => {
   const state = useContext(GlobalState);
   const [user] = state.UserAPI.User;
   const [pickupMachines] = state.MachinePickUpAPI.machinePickups;
-  const [PickupCallback, setPickupCallback] = state.MachinePickUpAPI.callback;
+  // const [PickupCallback, setPickupCallback] = state.MachinePickUpAPI.callback;
   const [pickupMachine, setPickupMachine] = useState("");
 
   useEffect(() => {
@@ -89,7 +89,7 @@ const TheHeaderDropdownMaint = () => {
         )
       );
     }
-    setPickupCallback(!PickupCallback);
+    // setPickupCallback(!PickupCallback);
   }, [pickupMachines, user]);
 
   // console.log(pickupMachine);
