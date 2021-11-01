8c84af364abb4ccd0db23dc0036f6fa7c2df3053
unknown <you@example.com>
2021-11-01 22:34:03 +0300

    Static express compenent returned back

--- server.js
+++ server.js
@@ -40,9 +40,9 @@ mongoose.connect(
 );
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/public"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "public", "index.html"));
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
   });
 }
 
