b4ee32e670aea988cd3771c8694b830292bae5f6
unknown <you@example.com>
2021-11-02 07:59:57 +0300

    Public path heroku path t1

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
+app.use("client/public", express.static(path.join(__dirname, "/public")));
 
 //Routes
 app.use("/user", require("./routes/userRouter"));
@@ -39,12 +40,12 @@ mongoose.connect(
   }
 );
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+// app.use(express.static("client/build"));
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
