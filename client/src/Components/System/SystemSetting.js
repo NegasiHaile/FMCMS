6e4f27d954085cf03ee9304882e70d39ebb22a48
unknown <you@example.com>
2022-01-27 16:42:04 +0300

    sender email card content set to responsive

--- client/src/Components/System/SystemSetting.js
+++ client/src/Components/System/SystemSetting.js
@@ -24,46 +24,48 @@ function SystemSetting() {
           <h6>Sender email</h6>
         </CCardHeader>
         <CCardBody>
-          <table className="table table-sm borderless">
-            <tbody>
-              <tr>
-                <td>NegasiHaile19@gmail.com</td>
-                <td>Primary</td>
-                <td className="d-flex justify-content-end">
-                  <span>
-                    <CLink className="text-danger">
-                      <CTooltip content={`Remove this email.`}>
-                        <CIcon name="cil-x" />
-                      </CTooltip>
-                    </CLink>
-                  </span>
-                </td>
-              </tr>
-              <tr>
-                <td>Negasi.Haile@Horizontech.com</td>
-                <td>
-                  <CButton className="ghost">Make Primary</CButton>
-                </td>
-                <td className="d-flex justify-content-end">
-                  <span>
-                    <CLink className="text-danger">
-                      <CTooltip content={`Remove this email.`}>
-                        <CIcon name="cil-x" />
-                      </CTooltip>
-                    </CLink>
-                  </span>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-          <CInputGroup className="mb-4" size="sm">
-            <CInput className="r-b-none" required />
-            <CInputGroupAppend>
-              <CInputGroupText className="jptr-btn" role="button">
-                <CIcon name="cil-plus" /> Add email
-              </CInputGroupText>
-            </CInputGroupAppend>
-          </CInputGroup>
+          <div className=" table-responsive">
+            <table className="table table-sm borderless">
+              <tbody>
+                <tr>
+                  <td>NegasiHaile19@gmail.com</td>
+                  <td>Primary</td>
+                  <td className="d-flex justify-content-end">
+                    <span>
+                      <CLink className="text-danger">
+                        <CTooltip content={`Remove this email.`}>
+                          <CIcon name="cil-x" />
+                        </CTooltip>
+                      </CLink>
+                    </span>
+                  </td>
+                </tr>
+                <tr>
+                  <td>Negasi.Haile@Horizontech.com</td>
+                  <td>
+                    <CButton className="ghost">Make Primary</CButton>
+                  </td>
+                  <td className="d-flex justify-content-end">
+                    <span>
+                      <CLink className="text-danger">
+                        <CTooltip content={`Remove this email.`}>
+                          <CIcon name="cil-x" />
+                        </CTooltip>
+                      </CLink>
+                    </span>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <CInputGroup className="mb-4" size="sm">
+              <CInput className="r-b-none" required />
+              <CInputGroupAppend>
+                <CInputGroupText className="jptr-btn" role="button">
+                  <CIcon name="cil-plus" /> Add email
+                </CInputGroupText>
+              </CInputGroupAppend>
+            </CInputGroup>
+          </div>
         </CCardBody>
       </CCard>
     </CCardGroup>
